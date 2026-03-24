e8431a9b33140f968d3f358350fc0b93cf7f80f3
DMayrant <DMayrant@icloud.com>
2026-03-24 17:26:38 +0700

    CI/CD pipeline update

--- S3State.tf
+++ S3State.tf
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "tf_state" {
     Name = "tf-state"
   }
 }
+############################
+# S3 Server Side Encryption
+############################
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 #########################
 # Block Public Access
 #########################
